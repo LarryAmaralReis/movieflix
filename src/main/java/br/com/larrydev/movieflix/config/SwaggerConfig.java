c859099b37c996a863da238c102a90e2bf6b34b3
Larry Amaral Reis <98787617+LarryAmaralReis@users.noreply.github.com>
2025-12-16 20:10:52 -0300

    feat: add routes swagger and api-docs

--- src/main/java/br/com/larrydev/movieflix/config/SwaggerConfig.java
+++ src/main/java/br/com/larrydev/movieflix/config/SwaggerConfig.java
@@ -1,4 +1,29 @@
 package br.com.larrydev.movieflix.config;
 
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI getOpenAPI() {
+
+        Info info = new Info()
+                .title("MovieFlix API")
+                .description("API for MovieFlix application")
+                .version("1.0.0")
+                .license(new License()
+                        .name("Apache 2.0")
+                        .url("http://springdoc.org"))
+                .contact(new Contact()
+                        .name("Larry Dev")
+                        .email("contato@larrydev.com.br"));
+
+        return new OpenAPI().info(info);
+    }
 }
